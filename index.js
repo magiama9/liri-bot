e2c7d15160652e28bf30e4658257d1b8d572aa01
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-01-23 14:55:41 -0500

    Move functionality from redundant homework and begin adding axios calls

---
+++ index.js
@@ -0,0 +1,82 @@
+/**************************************
+ * *******************************
+ * NODE DEPENDENCIES/REQUIREMENTS
+ * *******************************
+ **************************************/
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require("fs");
+const moment = require("moment");
+const dotenv = require("dotenv").config();
+const keys = require("./keys");
+const spotify = new Spotify(keys.spotify);
+
+inquirer
+  .prompt([
+    {
+      // LIST OF CHOICES FOR SEARCH PARAMETERS
+      type: "list",
+      message: "What would you like to search for?",
+      name: "searchParam",
+      choices: ["Movie", "Song or Artist", "Concert", "Random"]
+    },
+    {
+      // USER INPUT TO SEARCH
+      type: "input",
+      message: "What would you like to search for?",
+      name: "userSearch"
+    }
+  ])
+  .then(function(response) {
+    // SET RESPONSES FOR MANIPULATION
+    console.log(response);
+    let searchParam = response.searchParam;
+    let userSearch = response.userSearch;
+  });
+
+function searchSpotify(str) {
+  axios
+    .get("https://api.github.com/users/" + str)
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function searchConcert(str) {
+
+  // BANDS IN TOWN API CALL
+  // UTILIZES CODING BOOTCAMP APP_ID
+  axios
+    .get("https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp" + str)
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function searchMovie(str) {
+  axios
+    .get("https://api.github.com/users/" + str)
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function searchRandom(str) {
+  axios
+    .get("https://api.github.com/users/" + str)
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
